7001346c8eca609a50588d5452779af9c0e4a972
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-02-27 07:12:23 +0500

    updated for running https on server

--- AllAuthSocialLogins/settings.py
+++ AllAuthSocialLogins/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    #for https
+    'django_extensions',
 
     #local
     'user',
